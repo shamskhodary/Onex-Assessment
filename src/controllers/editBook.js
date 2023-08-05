fb19c52cc506fe9b01c2f7787f737765c8a0278d
shamskhodary <shams.khodary@gmail.com>
2023-08-05 12:06:52 +0300

    validate edit book inputs

--- src/controllers/editBook.js
+++ src/controllers/editBook.js
@@ -1,9 +1,11 @@
 import { editBookQuery } from "../queries/book.js";
-
+import validate from "../validation/addBookValidate.js";
 const editBook = async (req, res, next) => {
   try {
     const { id } = req.params;
     const { title, author, description, imageUrl, pages } = req.body;
+
+    await validate({ title, author, description, imageUrl, pages });
 
     const [data, updated] = await editBookQuery({
       title,
