d5b29ba65076ae81853ca6478a735bcbd15f9093
shamskhodary <shams.khodary@gmail.com>
2023-08-09 01:45:03 +0300

    Handling edit and delete book

--- client/src/components/Book/BookForm.vue
+++ client/src/components/Book/BookForm.vue
@@ -1,5 +1,59 @@
 <script setup>
+import swal from '../../helpers/swal'
+import axios from 'axios'
+import { ref } from 'vue'
+
+const popup = ref(false)
+const title = ref('')
+const author = ref('')
+const description = ref('')
+const imageUrl = ref('')
+const pages = ref(0)
+
+async function handleAddBook() {
+  popup.value = false
+  const bookInfo = {
+    title: title.value,
+    author: author.value,
+    description: description.value,
+    imageUrl: imageUrl.value,
+    pages: pages.value
+  }
+
+  const { data } = await axios.post('/api/v1/books', bookInfo)
+  if (data.status === 'err') {
+    popup.value = true
+    swal('error', data.message)
+  } else {
+    swal('success', data.message)
+  }
+}
+
+const rules = [
+  (value) => {
+    if (value) return true
+
+    return 'This field is required'
+  }
+]
 </script>
 
 <template>
+  <v-dialog v-model="popup" activator="parent" width="auto">
+    <v-card style="text-align: center; padding-top: 1rem">
+      <v-card-title>
+        <h2>Add a new book</h2>
+      </v-card-title>
+      <v-form @submit.prevent>
+        <v-text-field v-model="title" :rules="rules" label="Title"></v-text-field>
+        <v-text-field v-model="author" :rules="rules" label="Author"></v-text-field>
+        <v-text-field v-model="description" :rules="rules" label="Description"></v-text-field>
+        <v-text-field v-model="imageUrl" :rules="rules" label="Image Link"></v-text-field>
+        <v-text-field v-model="pages" :rules="rules" label="Pages"></v-text-field>
+      </v-form>
+      <v-card-actions>
+        <v-btn color="rgb(13, 214, 184)" @click="handleAddBook">Add</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
---
+++ client/src/components/Book/EitBook.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  popup: Boolean,
+  handleEditBook: Function
+})
+
+const editedBook = ref({
+  title: '',
+  author: '',
+  description: '',
+  imageUrl: '',
+  pages: ''
+})
+
+</script>
+<template>
+  <v-dialog v-if="popup" activator="parent" width="auto">
+    <v-card style="text-align: center; padding-top: 1rem">
+      <v-card-title>
+        <h2>Edit book</h2>
+      </v-card-title>
+      <v-form>
+        <v-text-field v-model="editedBook.title" label="Title"></v-text-field>
+        <v-text-field v-model="editedBook.author" label="Author"></v-text-field>
+        <v-text-field v-model="editedBook.description" label="Description"></v-text-field>
+        <v-text-field v-model="editedBook.imageUrl" label="Image Link"></v-text-field>
+        <v-text-field v-model="editedBook.pages" label="Pages"></v-text-field>
+      </v-form>
+      <v-card-actions>
+        <v-btn color="rgb(13, 214, 184)" @click="handleEditBook">Edit</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
